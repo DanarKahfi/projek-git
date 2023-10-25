df7ab57faf9417a8484ecf41eed7ecc7c4d87148
Danareee <muhdankah22@gmail.com>
2023-10-25 17:22:38 +0700

    ADD NEW VARIABLE

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ console.log("Aku Danar");
 console.log("Aku pupa");
 console.log("Aku cimenk pacarnya pupa");
 console.log("Aku cimenk");
+console.log("wkwkwkkwwk");
+
+const User = () => {
+    return (
+      <div>M Danar Kahfi</div>
+    )
+  }
+  
+  export default User
