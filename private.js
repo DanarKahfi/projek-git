af9ab063cbf656f88069df12a292f8803dc00f2e
Danareee <muhdankah22@gmail.com>
2023-10-25 15:19:11 +0700

    feat: add stash 1

---
+++ private.js
@@ -0,0 +1,17 @@
+console.log("HELLO JUA !!!");
+console.log("HELLO INI JUA LAGIII !!!");
+console.log("HELLO !!!");
+console.log("HELLO JUA !!!");
+console.log("HELLO INI JUA LAGIII !!!");
+console.log("HELLO !!!");
+console.log(" Hello I am Fauzi again!");
+console.log(" Hello I am Fauzi");
+console.log("cecil");
+console.log("ReynerWL");
+console.log("HI Rey");
+console.log(" Hello I am Fauzi");
+console.log("cecil");
+console.log(" Hello I am Danar");
+console.log("Danar");
+console.log("dignitas");
+
